be2161390845e55baa46dc8fea2c5785d16c1c46
SiuSiu <patrick.siu@aboutknowledge.com>
2024-09-30 16:44:18 +0800

    Update Exercises

--- src/ExercisesList.js
+++ src/ExercisesList.js
@@ -14,16 +14,12 @@ const ExercisesList = () => {
       <button style={{ width: "150px", height: "40px" }} onClick={() => setIsOpen1(!isOpen1)}>SCAA</button>
       {isOpen1 && (
         <div style={{ border: '1px solid black', margin: "10px 100px 10px 100px"}}>
-          <p>Leg Extension 10下 45~64 <input type="checkbox" /></p>
-          <p>Angled Leg Press 10下 25kg 50kg 75kg <input type="checkbox" /></p>
-          <p>Rack Squat 10下 20 30 40 <input type="checkbox" /></p>
+          <p>Rack Squat 10下 20 30 <input type="checkbox" /></p>
           <p>RDL 10下 10kg 15kg <input type="checkbox" /></p>
           <p>Lat Pull Down 10下 32kg 39kg <input type="checkbox" /></p>
-          <p>Seated Row 10下 25kg 30kg 35kg <input type="checkbox" /></p>
-          <p>Cable Tricep Extension 10下 5 6 7 <input type="checkbox" /></p>
-          <p>Single Leg Seated Leg Press 10下 90~130 <input type="checkbox" /></p>
-          <p>Double Leg Seated Leg Press 10下 170~210 <input type="checkbox" /></p>
-          <p>Single Leg Extension 10下 27kg <input type="checkbox" /></p>
+          <p>Seated Row 10下 25kg 30kg <input type="checkbox" /></p>
+          <p>Cable Tricep Extension 10下 5 6 <input type="checkbox" /></p>
+          <p>Leg Extension Combo 30 20lbs 10 100lbs <input type="checkbox" /></p>
         </div>
       )}
       <p>Home GYM</p>
